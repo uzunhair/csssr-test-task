4e7c3450606f86857cc097780e9d633809f30989
uzunhair <uzunhair@yandex.ru>
2019-07-08 09:58:09 +0300

    немного переделал js range

--- src/blocks/components/range/range.js
+++ src/blocks/components/range/range.js
@@ -6,19 +6,37 @@ const togglePosition = document.querySelector(".range__toggle-icon");
 
 for (i = 0; i < status.length; i++) {
 	status[i].onclick = function () {
-		const positionParent = parseInt(window.getComputedStyle(this).left);
-		const positionChildren = parseInt(window.getComputedStyle(this.children[0]).left);
-		const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetWidth);
-		const positionLine = 100/toggleContainerWidth * (positionParent + positionChildren);
-		togglePosition.style.left = `${positionLine}%`;
+
+		if (document.documentElement.clientWidth > 768) {
+			const positionParent = parseInt(window.getComputedStyle(this).left);
+			const positionChildren = parseInt(window.getComputedStyle(this.children[0]).left);
+			const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetWidth);
+			const positionLine = 100/toggleContainerWidth * (positionParent + positionChildren);
+			togglePosition.style.left = `${positionLine}%`;
+
+		} else {
+			const positionParent = parseInt(window.getComputedStyle(this).top);
+			const positionChildren = parseInt(window.getComputedStyle(this.children[0]).top);
+			const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetHeight);
+			const positionLine = 100/toggleContainerWidth * (positionParent + positionChildren);
+			togglePosition.style.top = `${positionLine}%`;
+		}
 	};
 }
 
 const toggle = document.querySelector(".range__toggle");
 
 toggle.onclick = function (e) {
-	const cursorPosition = e.offsetX;
-	const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetWidth);
-	const positionLine = 100/toggleContainerWidth * cursorPosition;
-	togglePosition.style.left = `${positionLine}%`;
+
+	if (document.documentElement.clientWidth > 768) {
+		const cursorPosition = e.offsetX;
+		const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetWidth);
+		const positionLine = 100 / toggleContainerWidth * cursorPosition;
+		togglePosition.style.left = `${positionLine}%`;
+	} else {
+		const cursorPosition = e.offsetY;
+		const toggleContainerWidth = parseInt(document.querySelector('.range__toggle').offsetHeight);
+		const positionLine = 100 / toggleContainerWidth * cursorPosition;
+		togglePosition.style.top = `${positionLine}%`;
+	}
 };
